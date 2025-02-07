d97d51ebbe7040397bc437476fb294aba6160eed
Rafael B. Biz <rafael.biz@netzsch.com>
2025-02-07 08:54:27 -0300

    Go: Getting Started - Multi Modules

--- go/getting-started/8.multi-module/workspace/example/hello/reverse/int.go
+++
@@ -1,9 +0,0 @@
-package reverse
-
-import "strconv"
-
-// Int returns the decimal reversal of the integer i.
-func Int(i int) int {
-	i, _ = strconv.Atoi(String(strconv.Itoa(i)))
-	return i
-}
--- go/getting-started/8.multi-module/workspace/hello/go.mod
+++
@@ -1,5 +0,0 @@
-module getting-started/hello
-
-go 1.23.4
-
-require golang.org/x/example/hello v0.0.0-20241216154601-40afcb705d05 // indirect
